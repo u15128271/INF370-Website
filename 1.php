c2da2670e9381c9d700f696a0c1f6fcd72660b59
u15128271 <itumeleng.morei@gmail.com>
2019-09-03 10:30:55 +0200

    website

---
+++ 1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Search</title>
+</head>
+<body>
+	<form method="post">
+		<label>Search</label>
+		<input type="text" name="Search">
+		<input type="submit" name="submit">
+	</form>
+</body>
+</html>
